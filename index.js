c2d1eb7e4844ddfd402b850bf216262e720a7435
Arkessiah <amarotoenriquez@gmail.com>
2021-11-27 19:32:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Dada una variable superheroes que sea un array con algunos heroes dentro.
+var superheroes = ['Spiderman', 'Hulk']
+
+// Crea ahora una función empujarHeroe que reciba un argumento arrayHeroes (será un array) y otro argumento nuevoHeroe (será un string) y empuje dentro de arrayHeroes el argumento nuevoHeroe.
+
+
+// Invoca a la función empujarHeroe enviando el array superheroes y el string "Thor".
+
+
+
+// Invoca a la función empujarHeroe enviando el array superheroes y el string "Iron-Man".
+
+
+// Crea ahora una función sacarHeroe que reciba un argumento arrayHeroes (será un array) y saque de este array el último elemento (¡recuerda la sección de arrays del prework!).
+
+
+// Invoca a la función sacarHeroe enviando el array superheroes
